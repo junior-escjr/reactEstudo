81f89020d0f90aabf657a78f000cfa32e9ea0045
junior_escjr <junior_escjr@yahoo.com.br>
2023-07-15 22:47:47 -0300

    finalizado o accordion

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -4,10 +4,20 @@ import { GoChevronDown, GoChevronRight } from "react-icons/go";
 const Accordion = ({ items }) => {
     const [expandedIndex, setExpandedIndex] = useState(-1);
 
+    const handleClick = ( nextindex ) => {
+        setExpandedIndex( ( currentExpandedIndex ) => {
+            if( currentExpandedIndex === nextindex ) {
+                return -1;
+            } else {
+                return nextindex;
+            }
+        });
+    }
+
     const renderItems = items.map( (item, key) => {
         return(
             <div key={key}>
-                <h3 className="flex p-3 items-center justify-between cursor-pointer border-b bg-gray-50" onClick={() => setExpandedIndex( key )}>
+                <h3 className="flex p-3 items-center justify-between cursor-pointer border-b bg-gray-50" onClick={() => handleClick( key )}>
                     { item.label }
                     <span className="text-xl">{key === expandedIndex ? <GoChevronDown /> : <GoChevronRight />}</span>
                 </h3>
