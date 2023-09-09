446721d117156fdfde8a4441257790f746586e5f
junior_escjr <junior_escjr@yahoo.com.br>
2023-09-09 03:06:56 -0300

    task(comps): implementando sistema de rotas do zero

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,20 +1,10 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
+import Link from "./components/Link";
 
 const App = () => {
-    const [selection, setSelection] = useState( null );
-
-    const handleSelect = ( option ) => {
-        setSelection( option );
-    }
-
-    const options = [
-        {label: 'Blue', value: 'blue' },
-        {label: 'Green', value: 'green' },
-        {label: 'Red', value: 'red' },
-    ];
-
-    return <Dropdown options={options} value={selection} onChange={handleSelect} />;
+    return <div>
+        <Link to="/accordion">Accordion</Link>
+        <Link to="/dropdown">Dropdown</Link>
+    </div>;
 }
 
 export default App;
---
+++ comps/src/pages/DropdownPage.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Dropdown from "./components/Dropdown";
+
+const DropdownPage = () => {
+    const [selection, setSelection] = useState( null );
+
+    const handleSelect = ( option ) => {
+        setSelection( option );
+    }
+
+    const options = [
+        {label: 'Blue', value: 'blue' },
+        {label: 'Green', value: 'green' },
+        {label: 'Red', value: 'red' },
+    ];
+
+    return (
+        <div className="flex"> 
+            <Dropdown options={options} value={selection} onChange={handleSelect} />
+        </div>
+    );
+}
+
+export default DropdownPage;
