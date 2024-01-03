7c3bfa6d0c527504a5610aa38d22d9479ec72c79
junior_escjr <junior_escjr@yahoo.com.br>
2024-01-02 21:18:08 -0300

    task: implementando as setas do sort

--- comps/src/components/SortableTable.js
+++ comps/src/components/SortableTable.js
@@ -1,10 +1,26 @@
+import { useState } from "react";
+import { GoArrowSmallDown, GoArrowSmallUp } from 'react-icons/go';
 import Table from "./Table";
 
+
 const SortableTable = ( props ) => {
-    const { config } = props;
+    const { config, data } = props;
+    const [sortOrder, setSortOrder] = useState(null);
+    const [sortBy, setSortBy] = useState(null);
 
     const handleClick = label => {
-        console.log( label );
+        if( sortOrder === null) {
+            setSortOrder('asc');
+            setSortBy( label );
+ 
+        } else if( sortOrder === 'asc') {
+            setSortOrder('desc');
+            setSortBy( label );
+
+        } else if( sortOrder === 'desc') {
+            setSortOrder( null );
+            setSortBy( null );
+        }
     };
 
     const updatedConfig = config.map( column => {
@@ -14,11 +30,60 @@ const SortableTable = ( props ) => {
 
         return {
             ...column,
-            header: () => <th onClick={() => handleClick( column.label )}> {column.label} </th>
+            header: () => <th className="cursor-pointer hover:bg-gray-100" onClick={() => handleClick( column.label )}>
+                <div className="flex items-center">
+                    {getIcons( column.label, sortBy, sortOrder )}
+                    {column.label}
+                </div>
+            </th>
         }
     });
 
-    return <Table {...props} config={updatedConfig} />
+    let sortedData = data;
+    if( sortOrder && sortBy) {
+        const { sortValue } = config.find(column => column.label === sortBy);
+
+        sortedData = [...data].sort( (a, b) => {
+            const valueA = sortValue(a);
+            const valueB = sortValue(b);
+
+            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+
+            if( typeof valueA === 'string') {
+                return valueA.localeCompare( valueB ) * reverseOrder;
+            } else {
+                return ( valueA - valueB ) * reverseOrder;
+            }
+        });
+    }
+
+    const getIcons = ( label, sortBy, sortOrder ) => {
+        if( label !== sortBy ) {
+            return <div>
+                <GoArrowSmallUp />
+                <GoArrowSmallDown />
+            </div>;
+        }
+
+        if( sortOrder === null ) {
+            return <div>
+                <GoArrowSmallUp />
+                <GoArrowSmallDown />
+            </div>;
+
+        } else if( sortOrder === 'asc' ) {
+            return <div>
+                <GoArrowSmallUp />
+            </div>;
+
+        } else if( sortOrder === 'desc') {
+            return <div>
+                <GoArrowSmallDown />
+            </div>;
+        }
+    }
+
+    return <Table {...props} data={sortedData} config={updatedConfig} />
 }
 
 export default SortableTable;
